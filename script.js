0e6296ad1e079d98a897a8bbca26a8fd736efb75
regret546 <iregret546@gmail.com>
2023-09-28 23:38:54 +0800

    add new and list function

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ let list = [];
 userInput = prompt("What would you like to do?");
 
 while (userInput !== "quit") {
-  if (list.length !== 0) {
-    userInput = prompt("What else you like to do?");
+  if (userInput == "new") {
+    if (list.length == 0) {
+      userInput = prompt("New todo");
+      list.push(userInput);
+    } else {
+      userInput = prompt("What else do you want to do?");
+      list.push(userInput);
+    }
   }
-  list.push(userInput);
+  if (userInput == "list") {
+    if (list.length == 0) {
+      console.log("There's no todo listed at the moment");
+    } else {
+      console.log("**********Todo-List**********");
+      for (let todo of list) {
+        console.log(`${list.indexOf(todo)}:${todo}`);
+      }
+      console.log("****************************");
+    }
+  }
+  userInput = prompt("What would you like to do?");
 }
