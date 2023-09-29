8e0a0e16d8846d0b754ab5dbccc05b1dcd2c25ab
regret546 <iregret546@gmail.com>
2023-09-29 23:42:45 +0800

    finish project and currently for verification

--- script.js
+++ script.js
@@ -3,23 +3,35 @@ userInput = prompt("What would you like to do?");
 
 while (userInput !== "quit") {
   if (userInput == "new") {
-    if (list.length == 0) {
-      userInput = prompt("New todo");
-      list.push(userInput);
-    } else {
-      userInput = prompt("What else do you want to do?");
-      list.push(userInput);
-    }
+    userInput = prompt("Enter new todo");
+    list.push(userInput);
+    console.log(`${userInput} is added to the list`);
   }
   if (userInput == "list") {
     if (list.length == 0) {
       console.log("There's no todo listed at the moment");
     } else {
-      console.log("**********Todo-List**********");
+      console.log("*********Todo-List*********");
       for (let todo of list) {
         console.log(`${list.indexOf(todo)}:${todo}`);
       }
-      console.log("****************************");
+      console.log("**************************");
+    }
+  }
+
+  if (userInput == "delete") {
+    if (list.length == 0) {
+      console.log("There's nothing to delete at the moment");
+    } else {
+      console.log("Which one do you want to delete?");
+      console.log("*********Todo-List*********");
+      for (let todo of list) {
+        console.log(`${list.indexOf(todo)}:${todo}`);
+      }
+      console.log("**************************");
+      userInput = prompt("Which one do you want to delete?");
+      list.splice(userInput, 1);
+      console.log("Successfully deleted");
     }
   }
   userInput = prompt("What would you like to do?");
